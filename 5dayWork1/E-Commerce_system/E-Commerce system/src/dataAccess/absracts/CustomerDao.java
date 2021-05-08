589af9d99ce1fbe6acefe950587230331517b39a
Bedirhan <53793285+bedirhanbuyukoz@users.noreply.github.com>
2021-05-08 18:58:00 +0300

    Delete 5dayWork1/E-Commerce_system/E-Commerce system/src directory

--- 5dayWork1/E-Commerce_system/E-Commerce system/src/dataAccess/absracts/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package dataAccess.absracts;
-
-import entities.concretes.Customer;
-
-public interface CustomerDao {
-	void singUp(Customer customer);
-	void singIn(Customer customer);
-
-}
--- 5dayWork1/E-Commerce_system/E-Commerce system/src/entities/absracts/Entity.java
+++
@@ -1,5 +0,0 @@
-package entities.absracts;
-
-public interface Entity {
-
-}

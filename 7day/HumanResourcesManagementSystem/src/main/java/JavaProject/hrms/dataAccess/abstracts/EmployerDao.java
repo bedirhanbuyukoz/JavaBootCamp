c1d171aea4d9f4fd10a4e698430cd1a65f4e13f4
Bedirhan <53793285+bedirhanbuyukoz@users.noreply.github.com>
2021-05-27 14:46:25 +0300

    Delete 7day directory

--- 7day/HumanResourcesManagementSystem/src/main/java/JavaProject/hrms/dataAccess/abstracts/EmployerDao.java
+++
@@ -1,9 +0,0 @@
-package JavaProject.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import JavaProject.hrms.entities.concretes.Employer;
-
-public interface EmployerDao extends JpaRepository<Employer, Integer> {
-
-}
--- 7day/HumanResourcesManagementSystem/src/main/java/JavaProject/hrms/dataAccess/abstracts/JobSeekerDao.java
+++
@@ -1,9 +0,0 @@
-package JavaProject.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import JavaProject.hrms.entities.concretes.JobSeeker;
-
-public interface JobSeekerDao extends JpaRepository<JobSeeker, Integer> {
-	JobSeeker getByNationalityId(String nationalityId);
-}

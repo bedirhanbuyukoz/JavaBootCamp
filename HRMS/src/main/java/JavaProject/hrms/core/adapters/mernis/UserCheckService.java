656e88168861c0c66a3cc14d2ba1ad7e55962798
Bedirhan <53793285+bedirhanbuyukoz@users.noreply.github.com>
2021-05-27 14:50:26 +0300

    Delete HRMS directory

--- HRMS/src/main/java/JavaProject/hrms/core/adapters/mernis/UserCheckService.java
+++
@@ -1,7 +0,0 @@
-package JavaProject.hrms.core.adapters.mernis;
-
-public interface UserCheckService {
-
-	boolean validate(String nationalityId, String firstName, String lastName, int birthYear);
-
-}
--- HRMS/src/main/java/JavaProject/hrms/dataAccess/abstracts/UserDao.java
+++
@@ -1,9 +0,0 @@
-package JavaProject.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import JavaProject.hrms.entities.concretes.User;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-	User getByEmail(String email);
-}
